19b2d57f4cc708917a9b88f2cab128d0fae1b158
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2023-02-06 17:17:30 -0500

    finished javascript code

--- script/main.js
+++ script/main.js
@@ -10,7 +10,11 @@
 
 let theButtons = document.querySelectorAll("#buttonHolder img"),
 	theHeading = document.querySelector("#headLine h1"),
-	puzzleBoard = document.querySelector(".puzzle-board");
+	puzzleBoard = document.querySelector(".puzzle-board"),
+	puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+	dropZones = document.querySelectorAll(".drop-zone"),
+	draggedPiece;
+	// theNavEl = document.querySelector("a");
 
 // Step 3
 // functionality goes in the middle => how do we want the app to behave
@@ -28,7 +32,24 @@ function changeBGImage() {
 	puzzleBoard.style.backgroundImage = `url('images/backGround${this.id}.jpg')`;
 }
 
+function handleStartDrag() {
+	console.log('Started dragging this piece:', this);
 
+	// store a reference to the puzzle piece image that we're dragging so we can use it later and move it to a drop zone
+	draggedPiece = this;
+}
+
+function handleDragOver(e) {
+	e.preventDefault();
+	console.log('dragged over me');
+	// this line is going to move the dragged piece from the left side of the board info whatever drop zone we choose. appendChild means "add element to the containter"
+	this.appendChild(draggedPiece);
+}
+
+function handleDrop(e) {
+	e.preventDefault();
+	console.log('dropped something on me');
+}
 // Step 2
 // event handling always go at the bottom => how do we want users to interact with our app
 
@@ -39,3 +60,22 @@ function changeBGImage() {
 //add event handling to each button in the collection of buttons, one at a time
 theButtons.forEach(button => button.addEventListener ("click", changeBGImage));
 //puzzleBoard.addEventListener("click, ")
+
+// add the drage event handling to the puzzle pieces
+puzzlePieces.forEach(piece => piece.addEventListener('dragstart', handleStartDrag));
+
+// add the dragover AND the drop event handling to the drop zones
+dropZones.forEach(zone => zone.addEventListener("dragover", handleDragOver));
+
+// add the drop event handling
+dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
+
+// function blockDefault(e) {
+// 	// block the default behavior of certain elements (a tags. forms, etc)
+// 	e.preventDefault();
+
+// 	console.log('do whatever we want instead');
+// }
+
+// theNavEl.addEventListener('click', blockDefault);
+
